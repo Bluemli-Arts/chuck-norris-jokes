049ed8ce6379c5689a9b7ce978eda6075efef788
Bluemli-Arts <94386655+Bluemli-Arts@users.noreply.github.com>
2024-08-31 17:50:31 +0200

    dist

--- src/components/Joke.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function Joke({joke}) {
-  return (
-    <div className="joke">
-      <img src={joke.icon_url} alt="Icon showing the head of Chuck Norris" />
-      <p>{joke.value}</p>
-    </div>
-  );
-}
